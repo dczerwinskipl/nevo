c6e959f20a850f52ddd49129894bc2c5d11e80f4
Dominik Czerwiński <dominikczerwinski@gmail.com>
2024-03-17 14:08:32 +0100

    Remove files with old naming

--- src/NEvo.Messaging.CQRS/Commands/ICommandDispatcher.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace NEvo.Messaging.Cqrs.Commands;
-
-public interface ICommandDispatcher
-{
-    Task DispatchAsync(Command command, CancellationToken cancellationToken);
-}
--- src/NEvo.Messaging.CQRS/Commands/ICommandHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿using LanguageExt;
-
-namespace NEvo.Messaging.Cqrs.Commands;
-
-public interface ICommandHandler<in TMessage> where TMessage : Command
-{
-    Task<Either<Exception, Unit>> HandleAsync(TMessage message, IMessageContext messageContext, CancellationToken cancellationToken);
-}
